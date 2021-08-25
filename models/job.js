7c2c013fcd692ca508f20e00e8d7272d1fd34eb7
acmenes <acmenescompositions@gmail.com>
2021-08-25 17:17:44 -0400

    fixed job routes

--- models/job.js
+++ models/job.js
@@ -12,12 +12,27 @@ class Job {
     /** Find all jobs */
     static async findAll() {
         const jobsRes = await db.query(
-            `SELECT title,
+            `SELECT id,
+                title,
                 salary,
                 equity,
                 company_handle
             FROM jobs`);
     return jobsRes.rows;
+    }
+
+    static async get(id){
+        const jobRes = await db.query(
+            `SELECT id, 
+                title, 
+                salary, 
+                equity, 
+                company_handle 
+            FROM jobs 
+            WHERE id = $1`, [id],
+        )
+    const job = jobRes.rows;
+    return job
     }
 }
 
