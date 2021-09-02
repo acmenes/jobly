cd8fca8ff68ad3d198bba47d4c0c64b93a1b36ab
acmenes <acmenescompositions@gmail.com>
2021-09-01 20:16:09 -0400

    updated company route

--- routes/companies.js
+++ routes/companies.js
@@ -6,11 +6,12 @@ const jsonschema = require("jsonschema");
 const express = require("express");
 
 const { BadRequestError } = require("../expressError");
-const { ensureLoggedIn } = require("../middleware/auth");
+const { ensureLoggedIn, ensureAdmin } = require("../middleware/auth");
 const Company = require("../models/company");
 
 const companyNewSchema = require("../schemas/companyNew.json");
 const companyUpdateSchema = require("../schemas/companyUpdate.json");
+const companySearchSchema = require("../schemas/companySearch.json")
 
 const router = new express.Router();
 
@@ -51,8 +52,17 @@ router.post("/", ensureLoggedIn, async function (req, res, next) {
  */
 
 router.get("/", async function (req, res, next) {
+  const query = req.query;
+  if (query.minEmployees !== undefined) query.minEmployees = + query.minEmployees
+  if (query.maxEmployees !== undefined) query.maxEmployees = + query.maxEmployees
+  
   try {
-    const companies = await Company.findAll();
+    const validator = jsonschema.validate(query, companySearchSchema)
+    if(!validator.valid) {
+      const errs = validator.errors.map(e => e.stack)
+      throw new BadRequestError(errs)
+    }
+    const companies = await Company.findAll(query)
     return res.json({ companies });
   } catch (err) {
     return next(err);
@@ -98,7 +108,7 @@ router.get("/:handle", async function (req, res, next) {
  * Authorization required: login
  */
 
-router.patch("/:handle", ensureLoggedIn, async function (req, res, next) {
+router.patch("/:handle", ensureAdmin, async function (req, res, next) {
   try {
     const validator = jsonschema.validate(req.body, companyUpdateSchema);
     if (!validator.valid) {
