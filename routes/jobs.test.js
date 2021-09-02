b2c01a688b5b2b89da5dd907f11722bf9faef849
acmenes <acmenescompositions@gmail.com>
2021-09-01 21:11:32 -0400

    added some tests for jobs routes

--- routes/jobs.test.js
+++ routes/jobs.test.js
@@ -11,6 +11,8 @@ const {
     commonAfterEach,
     commonAfterAll,
     u1Token,
+    testJobIds,
+    adminToken,
   } = require("./_testCommon");
 
 beforeAll(commonBeforeAll);
@@ -35,4 +37,34 @@ test("can post job", async function () {
         .send(newJob)
     expect(resp.statusCode).toEqual(201);
 })
+test("unauthorized for non-admin", async function (){
+    const resp = await request(app)
+        .post("/jobs")
+        .send(newJob)
+    expect(resp.statusCode).toEqual(403)
+})
+})
+
+/**************************************** GET /jobs */
+
+describe("GET /jobs", function () {
+
+})
+
+/**************************************** GET /job by id*/
+
+describe("GET /jobs/:id", function () {
+
+})
+
+/**************************************** PATCH /job by id */
+
+describe("PATCH /jobs/:id", function (){
+
+})
+
+/**************************************** DELETE /job by id */
+
+describe("DELETE /jobs/:id", function (){
+
 })
